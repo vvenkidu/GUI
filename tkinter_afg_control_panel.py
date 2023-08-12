46d6d7b877228353df78cd9b685a8ed4fdbc5ce6
Vikram Venkidu <103282365+vvenkidu@users.noreply.github.com>
2023-08-12 15:55:04 -0700

    Update tkinter_afg_control_panel.py

--- tkinter_afg_control_panel.py
+++ tkinter_afg_control_panel.py
@@ -6,25 +6,64 @@ Created on Fri Aug 11 12:42:44 2023
 """
 
 import tkinter as tk
+import pyvisa
+
+# establishing connection
+rm= pyvisa.ResourceManager()
+resources=rm.list_resources()
+print(resources)
+
+visa_address='USB0::0x0699::0x0347::C037742::0::INSTR'
+afg=rm.open_resource(visa_address,timeout=10000)
+idn=afg.query('*IDN?')
+print(idn)
+
+
+# listing SCPI commands
+
+
 
 root=tk.Tk()
 
+
+# Establish grid skeleton
 for i in range(0,10):
     for j in range(0,10):
         label=tk.Label(root,text="").grid(column=i,row=j, padx=10, pady=10)
+
+
+
+# Changing function type
+sin_command=':SOURce1:FUNCtion:SHAPe SIN'
+square_command=':SOURce1:FUNCtion:SHAPe SQU'
+ramp_command=':SOURce1:FUNCtion:SHAPe RAMP'
 
 sin_label=tk.Label(root,text="x").grid(column=1,row=1)
 squ_label=tk.Label(root,text="x").grid(column=1,row=2)
 ramp_label=tk.Label(root,text="x").grid(column=1,row=3)
 def sin_pressed():
-    
+    tk.Label(root,text="x").grid(column=1,row=1)
     tk.Label(root,text="o").grid(column=1,row=2)
     tk.Label(root,text="o").grid(column=1,row=3)
+    afg.write(sin_command)
+    
+
+def squ_pressed():
+    tk.Label(root,text="o").grid(column=1,row=1)
+    tk.Label(root,text="x").grid(column=1,row=2)
+    tk.Label(root,text="o").grid(column=1,row=3)
+    afg.write(square_command)
+
+def ramp_pressed():
+    tk.Label(root,text="o").grid(column=1,row=1)
+    tk.Label(root,text="o").grid(column=1,row=2)
+    tk.Label(root,text="x").grid(column=1,row=3)
+    afg.write(ramp_command)
     
 
 tk.Button(root, text="Sine", command=sin_pressed).grid(column=2,row=1)
-tk.Button(root, text="Square", command=sample_command).grid(column=2,row=2)
-tk.Button(root, text="Ramp", command=sample_command).grid(column=2,row=3)
+tk.Button(root, text="Square", command=squ_pressed).grid(column=2,row=2)
+tk.Button(root, text="Ramp", command=ramp_pressed).grid(column=2,row=3)
 
 
 
@@ -35,11 +74,11 @@ tk.Button(root, text="Ramp", command=sample_command).grid(column=2,row=3)
 DC_entry=tk.Entry(root)
 DC_entry.grid(column=2,row=5)
 
-tk.Label(root, text="DC Offset[V]").grid(column=1, row=5)
+tk.Label(root, text="DC Offset[mV]").grid(column=1, row=5)
 
 Amp_entry=tk.Entry(root)
 Amp_entry.grid(column=2,row=6)
-tk.Label(root, text="Amplitude[V]").grid(column=1, row=6)
+tk.Label(root, text="Amplitude[VPP]").grid(column=1, row=6)
 
 Freq_entry=tk.Entry(root)
 Freq_entry.grid(column=2, row=7)
@@ -49,14 +88,30 @@ Phi_entry=tk.Entry(root)
 Phi_entry.grid(column=2,row=8)
 tk.Label(root, text="Phi[deg]").grid(column=1, row=8)
 
+
 def OK_clicked():
     DC_input= DC_entry.get()
     Amp_input= Amp_entry.get()
     Freq_input= Freq_entry.get()
     Phi_input= Phi_entry.get()
+    
     user_input=DC_input or Amp_input or Freq_input or Phi_input
-    if user_input: 
+    if user_input:
+        if DC_input:
+            command=':SOURce1:VOLTage:LEVel:IMMediate:OFFSet '+DC_input+'mV'
+            afg.write(command)
+        if Amp_input:
+            command=':SOURce1:VOLTage:LEVel:IMMediate:AMPLitude '+Amp_input+'VPP'
+            afg.write(command)
+        if Freq_input:
+            command=':SOURce1:FREQuency:FIXed '+Freq_input+'HZ'
+            afg.write(command)
+        if Phi_input:
+            command=':SOURce1:PHASe:ADJust '+Phi_input+'DEG'
+            afg.write(command)
         print(DC_input, Amp_input, Freq_input, Phi_input)
+    
+    
 
 tk.Button(root, text="OK", command=OK_clicked).grid(column=8,row=8)
 
